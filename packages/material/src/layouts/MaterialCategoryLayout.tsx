2988d312b027ccdd6e172a68f5042313a3a16ba4
ga97waqZ <ga97waq@mytum.de>
2018-01-25 23:52:00 +0100

    commit to rebase

--- packages/material/src/layouts/MaterialCategoryLayout.tsx
+++ packages/material/src/layouts/MaterialCategoryLayout.tsx
@@ -9,27 +9,38 @@ import {
     rankWith,
     registerStartupRenderer,
     RendererProps,
-    uiTypeIs,
-    state
+    state,
+    uiTypeIs
 } from '@jsonforms/core';
 import { connect } from 'react-redux';
 import { MaterialLayoutRenderer, MaterialLayoutRendererProps } from './layout.util';
+import { JsonForms } from '../../../core/src/core';
 
 export const categoryLayoutTester: RankedTester = rankWith(1, uiTypeIs('CategoryLayout'));
+
+function TabContainer(props) {
+    return (
+        <Typography component="div" style={{ padding: 8 * 3 }}>
+            {props.children}
+        </Typography>
+    );
+}
+
 
 export class MaterialCategoryLayoutRenderer {
 
     constructor() {
         this.state = {
             selection: 0
-        }
+        };
     }
 
-    handleChange() {
+    handleChange(e) {
         this.setState({
-            selection:
-        })
+            selection: e.target.data
+        });
     }
+
 
     render() {
         const {data, schema, uischema, path, visible} = this.props;
@@ -48,15 +59,11 @@ export class MaterialCategoryLayoutRenderer {
                     <Tab label={JsonForms.stylingRegistry.getAsClassName('category.label')}/>
                 </Tabs>
             </AppBar>
-        {
-            data === 1 && <Typography component="div" style={{padding: 8 * 3}}>
+            <Typography component='div' style={{padding: 8 * 3}}>
                 <MaterialLayoutRenderer {...childProps}/>
-            </Typography>
-        }
-    )
-        ;
-    }
-};
+            </Typography>;
+    )}
+}
 
 export default registerStartupRenderer(
     categoryLayoutTester,
